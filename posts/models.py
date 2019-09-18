0fd5820338edb93a356033ebedd07398cd44b76b
Shree Ranga Raju <shreerangaraju@gmail.com>
2019-09-18 17:13:38 -0400

    refactor post model

--- posts/models.py
+++ posts/models.py
@@ -6,8 +6,8 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    post_text = models.TextField()
-    caption = models.TextField()
+    post_image = models.ImageField(null=True, blank=True)
+    caption = models.TextField(null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_by")
     created_at = models.DateTimeField(default=timezone.now)
 
@@ -15,16 +15,16 @@ class Post(models.Model):
         ordering = ("-created_at",)
 
     def __str__(self):
-        return self.post
+        return f"{self.post_image} by {self.owner.username}"
 
 
 # increase the like count or count the number of users liked it?
-class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_like")
-    # is_active = models.BooleanField(default=False)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_by")
-    created_at = models.DateTimeField(default=timezone.now)
+# class Like(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_like")
+#     # is_active = models.BooleanField(default=False)
+#     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_by")
+#     created_at = models.DateTimeField(default=timezone.now)
 
-    # class Meta:
-    #     unique_together = ("post", "owner")
+#     # class Meta:
+#     #     unique_together = ("post", "owner")
 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
-# Create your views here.
+from .serializers import PostSerializer
